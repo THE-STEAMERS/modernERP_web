3b4f8ecdbd535975cab5cdf1de3a8b799ff34388
venkatesh21bit <venkatesh.k21062005@gmail.com>
2025-01-07 19:24:40 +0530

    recent changes

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       </div>
       <span className="flex flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code sm:text-base text-sm font-medium">
         <TerminalSquareIcon className="w-5 h-5 sm:mr-1 mt-0.5" />
-        {"Founder:Venkatesh"}
+        {"Evolve"}
       </span>
     </div>
   );
--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -15,31 +15,16 @@ export const ROUTES: EachRoute[] = [
     items: [
       { title: "Introduction", href: "/introduction" },
       {
-        title: "Installation",
-        href: "/installation",
-      },
-      { title: "Quick Start Guide", href: "/quick-start-guide" },
-      {
         title: "Project Structure",
         href: "/project-structure",
       },
       {
-        title: "Components",
-        href: "/components",
-        items: [
-          { title: "Stepper", href: "/stepper" },
-          { title: "Tabs", href: "/tabs" },
-          { title: "Note", href: "/note" },
-          { title: "Code Block", href: "/code-block" },
-          { title: "Image & Link", href: "/image-link" },
-          { title: "Custom", href: "/custom" },
-        ],
+        title: "Installation",
+        href: "/installation",
       },
-      { title: "Themes", href: "/themes" },
-      {
-        title: "Customize",
-        href: "/customize",
-      },
+      { title: "Quick Start Guide", href: "/quick-start-guide" },
+      
+      
     ],
   },
 ];
